d60ad25f3d1bcddac1d3de429e0ac73711b5e51e
mamf <mamf20021381@>
2022-11-08 15:27:53 +0330

    Eighth Commit Throttled

--- BookManagement/settings.py
+++ BookManagement/settings.py
@@ -158,6 +158,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '30/minute',
+    }
 }
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Codinto',),
