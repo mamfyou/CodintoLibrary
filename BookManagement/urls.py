a1ac62d793b26e808015306719ad89bae55cdd94
mamf <mamf20021381@>
2023-03-23 12:22:21 +0330

    changing settings and urls.py

--- BookManagement/urls.py
+++ BookManagement/urls.py
@@ -1,23 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
+from django.urls import path, include
 
 urlpatterns = [
       path('', include('books.urls')),
@@ -25,9 +9,6 @@ urlpatterns = [
       path('', include('process.urls')),
       path('auth/', include('rest_framework.urls')),
       path('admin/', admin.site.urls),
-      path('__debug__/', include('debug_toolbar.urls')),
-      re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
-              name='schema-json'),
-      re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-      re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+      path('debug/', include('debug_toolbar.urls')),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +\
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
